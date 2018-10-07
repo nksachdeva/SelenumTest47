5101cd031887dd7328151fec694583a52760988e
nksachdeva <info.naveensachdeva@gmail.com>
2018-10-07 14:40:42 +0530

    First Commit

---
+++ SeleniumTest/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
+using NUnit.Framework;
+
+namespace SeleniumTest
+{
+    class Program
+    {
+        //Initialize driver
+        IWebDriver driver = new InternetExplorerDriver();
+
+        static void Main(string[] args)
+        {
+            
+        }
+
+        [SetUp]
+        public void Initialize()
+        {
+            driver.Navigate().GoToUrl("https://www.google.com/");
+            Console.WriteLine("Goto URL");
+        }
+
+        [Test]
+        public void Execute()
+        {
+            //Perform Operations
+            driver.FindElement(By.Name("q")).SendKeys("Selenium");
+            Console.WriteLine("Do a search");
+        }
+
+        [TearDown]
+        public void EndTest()
+        {
+            //Exit browser
+            driver.Close();
+            Console.WriteLine("Close Browser");
+
+        }
+    }
+}
